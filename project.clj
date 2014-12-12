cea50fd0c78e509044c825e7ca7ef892f00f9655
Toby James <toby.james@flybe.com>
2014-12-12 15:50:24 +0000

    Initial commit - brepl enabled

---
+++ project.clj
@@ -0,0 +1,28 @@
+(defproject modern-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  ;; clj and cljs source code path
+  :source-paths ["src/clj" "src/cljs"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2411"]]
+
+  ;; lein cljs build plugin to build a cljs project
+  :plugins [[lein-cljsbuild "1.0.3"]]
+
+  ;;cljsbuild options
+  :cljsbuild {:builds
+              [{;;cljs source code path
+                :source-paths ["src/cljs"]
+
+                ;;Google closure (cls) options configuration
+                :compiler {;;cls generated js script filename
+                           :output-to "resources/public/js/modern.js"
+
+                           ;; minimal js optimization directive
+                           :optimizations :whitespace
+
+                           ;;generated js code prettification
+                           :pretty-print true}}]})
